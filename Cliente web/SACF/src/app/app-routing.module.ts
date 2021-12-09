3d5f0479ffba68faa75409dc06be54640e2723a1
Bojorge <bojtronic@gmail.com>
2021-12-09 14:29:55 -0600

    Apariencia del login

--- Cliente web/SACF/src/app/app-routing.module.ts
+++ Cliente web/SACF/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'login', pathMatch:'full'}, //componente inicial (se redirecciona al login)
+  { path:'login', component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents=[LoginComponent]
