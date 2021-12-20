11302c48e58a3c26a61bf0829008fb132c881018
Bojorge <bojtronic@gmail.com>
2021-12-19 23:50:05 -0600

    Gráficas

--- Cliente web/SACF/src/app/app-routing.module.ts
+++ Cliente web/SACF/src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './Pages/dashboard/dashboard.component';
+import { PieChartComponent } from './Charts/pie-chart/pie-chart.component';
+import { LineChartComponent } from './Charts/line-chart/line-chart.component';
+import { BarChartComponent } from './Charts/bar-chart/bar-chart.component';
+import { DoughnutChartComponent } from './Charts/doughnut-chart/doughnut-chart.component';
+import { RadarChartComponent } from './Charts/radar-chart/radar-chart.component';
+import { PolarAreaChartComponent } from './Charts/polar-area-chart/polar-area-chart.component';
+import { BubbleChartComponent } from './Charts/bubble-chart/bubble-chart.component';
+import { ScatterChartComponent } from './Charts/scatter-chart/scatter-chart.component';
+import { DynamicChartComponent } from './Charts/dynamic-chart/dynamic-chart.component';
+import { FinancialChartComponent } from './Charts/financial-chart/financial-chart.component';
 
 const routes: Routes = [
-  {
-    path:"",
-    loadChildren:() => import("./modules/public/public.module").then(m=>m.PublicModule)
-  },
-  {
-    path:"dashboard",
-    loadChildren:() => import("./modules/dashboard/dashboard.module").then(m=>m.DashboardModule)
-  }
+  {path:'', redirectTo:'radarchart', pathMatch:'full'}, //componente inicial (se redirecciona al login)
+  { path:'radarchart', component:PieChartComponent}, //se idica la ruta para llegar o utilizar un componente
 ];
 
 @NgModule({
@@ -17,3 +22,8 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+//se exportan los componentes para que se puedan redireccionar
+export const routingComponents=[DashboardComponent, PieChartComponent, LineChartComponent, BarChartComponent,
+  DoughnutChartComponent, RadarChartComponent, PolarAreaChartComponent, BubbleChartComponent, ScatterChartComponent,
+  DynamicChartComponent, FinancialChartComponent]
--- Cliente web/SACF/src/app/app.module.ts
+++ Cliente web/SACF/src/app/app.module.ts
@@ -4,15 +4,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './Pages/dashboard/dashboard.component';
+
+import { NgChartsModule } from 'ng2-charts';
+import { PieChartComponent } from './Charts/pie-chart/pie-chart.component';
+import { LineChartComponent } from './Charts/line-chart/line-chart.component';
+import { BarChartComponent } from './Charts/bar-chart/bar-chart.component';
+import { DoughnutChartComponent } from './Charts/doughnut-chart/doughnut-chart.component';
+import { RadarChartComponent } from './Charts/radar-chart/radar-chart.component';
+import { PolarAreaChartComponent } from './Charts/polar-area-chart/polar-area-chart.component';
+import { BubbleChartComponent } from './Charts/bubble-chart/bubble-chart.component';
+import { ScatterChartComponent } from './Charts/scatter-chart/scatter-chart.component';
+import { DynamicChartComponent } from './Charts/dynamic-chart/dynamic-chart.component';
+import { FinancialChartComponent } from './Charts/financial-chart/financial-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    PieChartComponent,
+    LineChartComponent,
+    BarChartComponent,
+    DoughnutChartComponent,
+    RadarChartComponent,
+    PolarAreaChartComponent,
+    BubbleChartComponent,
+    ScatterChartComponent,
+    DynamicChartComponent,
+    FinancialChartComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
